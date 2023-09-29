e02f8af32e3fd54db7e4deb0b748707618f32aa9
Eric Eastwood <contact@ericeastwood.com>
2023-09-29 03:15:54 -0500

    Read training images

---
+++ src/big_endian_struct_deserializer.zig
@@ -0,0 +1,42 @@
+const std = @import("std");
+
+// Based on https://nathancraddock.com/blog/2022/deserialization-with-zig-metaprogramming/#writing-a-struct-deserializer-with-zig
+pub fn BigEndianStructDeserializer(comptime ReaderType: type) type {
+    return struct {
+        reader: ReaderType,
+
+        // We use our own `readStruct(...)` function instead of `self.reader.readStruct(...)`
+        // because the standard library uses a byte swap function that does not work on arrays.
+        fn readStruct(self: *@This(), comptime T: type) !T {
+            const fields = std.meta.fields(T);
+
+            var item: T = undefined;
+            inline for (fields) |field| {
+                @field(item, field.name) = try self.read(field.type);
+            }
+
+            return item;
+        }
+
+        pub fn read(self: *@This(), comptime T: type) !T {
+            return switch (@typeInfo(T)) {
+                .Int => try self.reader.readIntBig(T),
+                .Float => try self.reader.readVarInt(T, .Big, @sizeOf(T)),
+                .Array => |array| {
+                    var arr: [array.len]array.child = undefined;
+                    var index: usize = 0;
+                    while (index < array.len) : (index += 1) {
+                        arr[index] = try self.read(array.child);
+                    }
+                    return arr;
+                },
+                .Struct => try self.readStruct(T),
+                else => @compileError("unsupported type"),
+            };
+        }
+    };
+}
+
+pub fn bigEndianStructDeserializer(reader: anytype) BigEndianStructDeserializer(@TypeOf(reader)) {
+    return .{ .reader = reader };
+}
--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,12 @@
 const std = @import("std");
+const bigEndianStructDeserializer = @import("big_endian_struct_deserializer.zig").bigEndianStructDeserializer;
 
 const TRAIN_DATA_FILE_PATH = "data/train-images-idx3-ubyte";
 const TRAIN_LABELS_FILE_PATH = "data/train-labels-idx1-ubyte";
 const TEST_DATA_FILE_PATH = "data/t10k-images-idx3-ubyte";
 const TEST_LABELS_FILE_PATH = "data/t10k-labels-idx1-ubyte";
+
+const NUMBER_OF_IMAGES_TO_TRAIN_ON = 100;
 
 const MnistImageFileHeader = extern struct {
     magic_number: u32,
@@ -11,28 +14,78 @@ const MnistImageFileHeader = extern struct {
     number_of_rows: u32,
     number_of_columns: u32,
 
-    pub fn asBuf(self: *@This()) []u8 {
-        return @as([*]u8, @ptrCast(self))[0..@sizeOf(@This())];
-    }
+    // pub fn asBuf(self: *@This()) []u8 {
+    //     return @as([*]u8, @ptrCast(self))[0..@sizeOf(@This())];
+    // }
 };
 
-// const Image = extern struct {
-//     [28 * 28]u8;
-// }
+const Image = extern struct {
+    pixels: [28 * 28]u8,
+};
+
+fn decorateStringWithAnsiColor(input_string: []const u8, hex_color: u24, allocator: std.mem.Allocator) ![]const u8 {
+    const string = try std.fmt.allocPrint(
+        allocator,
+        "\u{001b}[38;2;{d};{d};{d}m{s}\u{001b}[0m",
+        .{
+            // red
+            hex_color >> 16,
+            // green
+            (hex_color >> 8) & 0xFF,
+            // blue
+            hex_color & 0xFF,
+            input_string,
+        },
+    );
+    return string;
+}
 
 pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer {
+        switch (gpa.deinit()) {
+            .ok => {},
+            .leak => std.log.err("GPA allocator: Memory leak detected", .{}),
+        }
+    }
+
     const file = try std.fs.cwd().openFile(TRAIN_DATA_FILE_PATH, .{});
     defer file.close();
-    //try file.seekTo(0);
 
+    // Use a buffered reader for better performance (less syscalls)
+    // (https://zig.news/kristoff/how-to-add-buffering-to-a-writer-reader-in-zig-7jd)
     var buffered_reader = std.io.bufferedReader(file.reader());
     var file_reader = buffered_reader.reader();
 
     const header = try file_reader.readStructBig(MnistImageFileHeader);
     std.log.debug("header {}", .{header});
-
     try std.testing.expectEqual(header.magic_number, 2051);
     try std.testing.expectEqual(header.number_of_images, 60000);
     try std.testing.expectEqual(header.number_of_rows, 28);
     try std.testing.expectEqual(header.number_of_columns, 28);
+
+    // Make sure we don't try to allocate more images than there are in the file
+    std.debug.assert(header.number_of_images >= NUMBER_OF_IMAGES_TO_TRAIN_ON);
+    const image_array = try allocator.alloc(Image, NUMBER_OF_IMAGES_TO_TRAIN_ON);
+    defer allocator.free(image_array);
+
+    var deserializer = bigEndianStructDeserializer(file_reader);
+
+    for (0..(NUMBER_OF_IMAGES_TO_TRAIN_ON - 1)) |image_index| {
+        const image = try deserializer.read(Image);
+        image_array[image_index] = image;
+    }
+
+    const colored_asdf = try decorateStringWithAnsiColor("asdf", 0xFF0000, allocator);
+    defer allocator.free(colored_asdf);
+    std.log.debug("asdf {s}", .{
+        colored_asdf,
+    });
+
+    for (0..(header.number_of_rows - 1)) |image_index| {
+        const start_index = image_index * header.number_of_columns;
+        const end_index = start_index + header.number_of_columns;
+        std.log.debug("{any}", .{image_array[0].pixels[start_index..end_index]});
+    }
 }
